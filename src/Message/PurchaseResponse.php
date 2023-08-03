e39aaa6be0cac6dd23e6e3a5679f984c38ade20c
trpl2alex14 <trpl2alex14@bk.ru>
2023-08-03 15:56:38 +0500

    feature - PurchaseResponse

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -1,20 +1,60 @@
 <?php
 
-
 namespace Omnipay\YooKassa\Message;
-
 
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
+use YooKassa\Model\ConfirmationType;
+use YooKassa\Model\PaymentInterface;
+use YooKassa\Model\PaymentStatus;
+
 
 class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
 {
+    public function getData(): PaymentInterface
+    {
+        return $this->data;
+    }
 
-    /**
-     * @inheritDoc
-     */
+
     public function isSuccessful()
     {
-        // TODO: Implement isSuccessful() method.
+        return !!$this->getData()->getId();
+    }
+
+
+    public function getTransactionId()
+    {
+        return $this->getData()->getId();
+    }
+
+
+    public function getRedirectUrl()
+    {
+        return $this->getData()->getConfirmation()->getConfirmationUrl();
+    }
+
+
+    public function getRedirectData()
+    {
+        return $this->getData()->getConfirmation()->getConfirmationData();
+    }
+
+
+    public function isPending()
+    {
+        return $this->getData()->getStatus() === PaymentStatus::PENDING;
+    }
+
+
+    public function isCancelled()
+    {
+        return $this->getData()->getStatus() === PaymentStatus::CANCELED;
+    }
+
+
+    public function isRedirect()
+    {
+        return $this->getData()->getConfirmation()->getType() === ConfirmationType::REDIRECT;
     }
 }
